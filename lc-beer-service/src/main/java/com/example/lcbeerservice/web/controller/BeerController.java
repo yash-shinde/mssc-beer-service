27897c4d84e8c2d6398b8048510cf527af48655c
yash-shinde <yashshinde52@gmail.com>
2023-12-25 21:11:59 +0530

    Service Added. Closes #3

---
+++ lc-beer-service/src/main/java/com/example/lcbeerservice/web/controller/BeerController.java
@@ -0,0 +1,33 @@
+package com.example.lcbeerservice.web.controller;
+
+import com.example.lcbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(("api/v1/beer"))
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(UUID beerId){
+        //todo impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId
+            ,@RequestBody BeerDto beerDto ){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+}
