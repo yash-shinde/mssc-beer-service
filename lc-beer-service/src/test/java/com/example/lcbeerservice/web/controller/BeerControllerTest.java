b975c017ef401dcd287b848f4ef049f905ef40bc
yash-shinde <yashshinde52@gmail.com>
2023-12-25 23:08:31 +0530

    Closes #3,closes #4. Tests

---
+++ lc-beer-service/src/test/java/com/example/lcbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package com.example.lcbeerservice.web.controller;
+
+import com.example.lcbeerservice.web.model.BeerDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mvcMock;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getBeerById() throws Exception {
+        mvcMock.perform(
+                get("/api/v1/beer/"+ UUID.randomUUID().toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveBeer() throws Exception {
+    BeerDto reqObj = BeerDto.builder().build();
+    String reqBody = objectMapper.writeValueAsString(reqObj);
+
+    mvcMock.perform(post("/api/v1/beer")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(reqBody))
+            .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+        BeerDto reqObj = BeerDto.builder().build();
+        String reqBody = objectMapper.writeValueAsString(reqObj);
+
+        mvcMock.perform(put("/api/v1/beer/"+UUID.randomUUID().toString())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(reqBody))
+                .andExpect(status().isNoContent());
+
+    }
+}
