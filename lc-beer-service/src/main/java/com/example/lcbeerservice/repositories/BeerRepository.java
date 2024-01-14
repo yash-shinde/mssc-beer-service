ae071716307d5408f5143ed99baae08530fc058a
yash-shinde <yashshinde52@gmail.com>
2024-01-14 16:27:16 +0530

    Closes #6

---
+++ lc-beer-service/src/main/java/com/example/lcbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.example.lcbeerservice.repositories;
+
+import com.example.lcbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
