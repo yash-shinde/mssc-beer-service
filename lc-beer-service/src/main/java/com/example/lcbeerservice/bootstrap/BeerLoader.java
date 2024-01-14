e95bab3a31e1fbfaf8ef26115b003c0210d76eeb
yash-shinde <yashshinde52@gmail.com>
2024-01-14 16:45:23 +0530

    Closes #7

---
+++ lc-beer-service/src/main/java/com/example/lcbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,65 @@
+package com.example.lcbeerservice.bootstrap;
+
+import com.example.lcbeerservice.domain.Beer;
+import com.example.lcbeerservice.repositories.BeerRepository;
+import com.example.lcbeerservice.web.model.BeerStyleEnum;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
+    private final BeerRepository beerRepo;
+
+    public BeerLoader(BeerRepository beerRepo) {
+        this.beerRepo = beerRepo;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(beerRepo.count() == 0 ) {
+            loadBeerObjects();
+        }
+    }
+
+    private void loadBeerObjects() {
+        Beer b1 = Beer.builder()
+                .beerName("Mango Bobs")
+                .beerStyle(BeerStyleEnum.IPA.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_1_UPC)
+                .build();
+
+        Beer b2 = Beer.builder()
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_2_UPC)
+                .build();
+
+        Beer b3 = Beer.builder()
+                .beerName("Pinball Porter")
+                .beerStyle(BeerStyleEnum.PALE_ALE.name())
+                .minOnHand(12)
+                .quantityToBrew(200)
+                .price(new BigDecimal("12.95"))
+                .upc(BEER_3_UPC)
+                .build();
+
+        beerRepo.save(b1);
+        beerRepo.save(b2);
+        beerRepo.save(b3);
+
+        System.out.println("beers saved::::"+beerRepo.count());
+    }
+}
--- lc-beer-service/src/main/java/com/example/lcbeerservice/repositories/BeerRepository.java
+++ lc-beer-service/src/main/java/com/example/lcbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,20 @@
 package com.example.lcbeerservice.repositories;
 
 import com.example.lcbeerservice.domain.Beer;
+import com.example.lcbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
 }
