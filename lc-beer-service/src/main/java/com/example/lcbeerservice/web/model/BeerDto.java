a5bbc133fb6a3692514d5611df79e0be55246fd6
yash-shinde <yashshinde52@gmail.com>
2025-07-29 00:50:53 +0530

    Added Validation closes#8

--- lc-beer-service/src/main/java/com/example/lcbeerservice/web/model/BeerDto.java
+++ lc-beer-service/src/main/java/com/example/lcbeerservice/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package com.example.lcbeerservice.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +19,29 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
     private UUID id;
+    @Null
     private String version;
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @NotBlank
+    private String beerName;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @Null
     private OffsetDateTime createdDate;
-    private OffsetDateTime modifyDate;
+    @Null
+    private OffsetDateTime lastModifiedDate;
+
     private Long isn;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 
 }
